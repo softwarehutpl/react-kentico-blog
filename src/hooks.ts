86a66c4f922d8d8c213da3a8bddd1f7b8668d7d2
Maciej Sopyło <maciek134@gmail.com>
2019-04-26 11:17:14 +0200

    feat: add single content item query hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -13,7 +13,13 @@ interface ListOptions {
   depth?: number;
   sort?: SortOrder;
   orderBy?: string;
+  page?: number;
+  pageSize?: number;
 }
+
+const listDefaults: ListOptions = {
+  pageSize: 20,
+};
 
 export function useList<T extends ContentItem>(model: string, options: ListOptions = {}): T[] {
   const client = useKentico();
@@ -28,6 +34,10 @@ export function useList<T extends ContentItem>(model: string, options: ListOptio
     if (options.orderBy) {
       request.orderParameter(options.orderBy, options.sort || SortOrder.asc);
     }
+    if (options.page >= 0) {
+      request.skipParameter(options.page * (options.pageSize || listDefaults.pageSize));
+      request.limitParameter(options.pageSize || listDefaults.pageSize);
+    }
 
     const subscription = request.getObservable().subscribe(
       /* istanbul ignore next: would test React and/or RxJS */
@@ -36,7 +46,38 @@ export function useList<T extends ContentItem>(model: string, options: ListOptio
 
     /* istanbul ignore next: would test React */
     return () => subscription.unsubscribe();
-  }, [model, options.depth, options.sort, options.orderBy]);
+  }, [model, options.depth, options.sort, options.orderBy, options.page, options.pageSize]);
 
   return list;
+}
+
+interface SingleOptions {
+  filterBy: string;
+}
+
+export function useSingle<T extends ContentItem>(
+  model: string,
+  key: string,
+  options: SingleOptions
+) {
+  const client = useKentico();
+  const [item, setItem] = useState<T>();
+
+  useEffect(() => {
+    const request = client
+      .items<T>()
+      .type(model)
+      .equalsFilter(`elements.${options.filterBy}`, key)
+      .limitParameter(1);
+
+    const subscription = request.getObservable().subscribe(
+      /* istanbul ignore next: would test React and/or RxJS */
+      res => setItem(res.firstItem)
+    );
+
+    /* istanbul ignore next: would test React */
+    return () => subscription.unsubscribe();
+  }, [model]);
+
+  return item;
 }
